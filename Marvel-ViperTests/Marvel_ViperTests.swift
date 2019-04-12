ec53ccd4d5a24bd1dc2d35b46cdffa446ae7def0
Alan Casas <alancasasarevalo@gmail.com>
2019-04-12 17:39:42 +0200

    [FIRST COMMIT]

---
+++ Marvel-ViperTests/Marvel_ViperTests.swift
@@ -0,0 +1,34 @@
+//
+//  Marvel_ViperTests.swift
+//  Marvel-ViperTests
+//
+//  Created by Alan Casas on 12/04/2019.
+//  Copyright © 2019 Alan Casas. All rights reserved.
+//
+
+import XCTest
+@testable import Marvel_Viper
+
+class Marvel_ViperTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
